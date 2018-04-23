f551bdde5be48b123adf09a921dd4d0c62ba8cc1
Ben Stone <ben.stone-sraha@fau.de>
2018-04-23 23:14:23 +0200

    Added license

--- src/main/angular/src/app/calendar/calendar-content/calendar-content.component.ts
+++ src/main/angular/src/app/calendar/calendar-content/calendar-content.component.ts
@@ -1,3 +1,26 @@
+/**
+ *  @license 
+ *  
+ * 
+ * Copyright [2018] [(MAMB Manuel HUbert, Marcel Werle, Artur Mandybura and Benjamin Stone)]
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Copyright (c) 2018 by MAMB (Manuel HUbert, Marcel Werle, Artur Mandybura and Benjamin Stone)
+ * 
+ * 
+ */
+
 import { Input,Component, OnInit }  from '@angular/core';
 import { ConstructionManager}       from '../../shared/construction-manager';
 import { CalendarWeekItemComponent} from '../calendar-week-item/calendar-week-item.component' ;
@@ -13,10 +36,7 @@ export class CalendarContentComponent implements OnInit {
   public constructionManagers  : Array<ConstructionManager>;
 
 
-  constructor( )
-  {
-    
-  }
+  constructor( ) { }
 
   ngOnInit()
   {
