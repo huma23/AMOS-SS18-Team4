08ff7fb9261ddf2975b2e95d72159b0802b44c96
Ben Stone <ben.stone-sraha@fau.de>
2018-04-23 20:40:04 +0200

    Changes on Main Calender Component: 	-> added Property calWeeks as Array on Component as Input Parameter 	-> calWeeks is received by the Calendar Store Service on Init 	-> delegete elements of the the calWeeks property to the Child 		CalenderWeek Component through Property Binding

--- src/main/angular/src/app/calendar/main-calendar/main-calendar.component.ts
+++ src/main/angular/src/app/calendar/main-calendar/main-calendar.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {CalendarContentComponent} from '../calendar-content/calendar-content.component';
-import {CalendarHeaderComponent } from '../calendar-header/calendar-header.component';
-
+import { Component, OnInit, Output }  from '@angular/core';
+import { CalendarContentComponent}    from '../calendar-content/calendar-content.component';
+import { CalendarHeaderComponent }    from '../calendar-header/calendar-header.component';
+import { CalenderStoreService}        from '../../shared/calender-store.service';
+import { CalendarWeekComponent}       from '../calendar-week/calendar-week.component';
+import { CalendarWeek }               from '../../shared/calender-week';
 
 @Component({
   selector: 'pl-main-calendar',
   templateUrl: './main-calendar.component.html',
   styleUrls: ['./main-calendar.component.css']
 })
-export class MainCalendarComponent implements OnInit {
+export class MainCalendarComponent implements OnInit
+{
+  
+  private calWeeks              : Array<CalendarWeek>;
 
-  constructor() { }
 
-  ngOnInit() {
+
+  constructor(private csService : CalenderStoreService) { } 
+
+  ngOnInit()
+  {
+    this.calWeeks = this.csService.getCalendarWeeks();
   }
 
 }
