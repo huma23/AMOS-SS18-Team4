840142223c9e51775e6bfe44ccb5d4220bc9bb4d
huma23 <manuel.hubert7@gmail.com>
2018-05-05 14:38:00 +0200

    Solved routing problem on app engine

---
+++ src/main/java/de/amos/mamb/apps/RoutingServlet.java
@@ -0,0 +1,18 @@
+package de.amos.mamb.apps;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RoutingServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        RequestDispatcher rd = req.getRequestDispatcher("/index.html");
+        rd.forward(req, resp);
+    }
+}
