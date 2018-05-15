21fe1a895555dd2d93ff30a9b23f9ca46c01734a
huma23 <manuel.hubert7@gmail.com>
2018-05-15 17:29:55 +0200

    adjustment of jackson version

--- src/main/java/de/amos/mamb/rest/command/Command.java
+++ src/main/java/de/amos/mamb/rest/command/Command.java
@@ -2,14 +2,7 @@ package de.amos.mamb.rest;
 
 public interface Command {
 
-    enum Result {
-        FAILED,
-        NO_OBJECT
-    };
-
     int httpOnSuccess();
 
     int httpOnCommandFailed();
-
-    Object execute();
 }
