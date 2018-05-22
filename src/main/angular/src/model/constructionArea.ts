5c43ac22825dcd5b877f4d5146afc8f97afda525
huma23 <manuel.hubert7@gmail.com>
2018-05-22 18:09:46 +0200

    Construction leader in right frontend panel

--- src/main/angular/src/model/constructionArea.ts
+++ src/main/angular/src/model/constructionArea.ts
@@ -3,5 +3,5 @@ import {ConstructionLadder} from "./constructionLadder";
 
 export class ConstructionArea implements IConstructionArea{
 
-  constructor(public name:string, public startDate:string, public endDate:string, public bauleiter: ConstructionLadder){}
+  constructor(public name:string, public startDate:string, public endDate:string, public bauleiter: ConstructionLadder, public  permanent:boolean){}
 }
